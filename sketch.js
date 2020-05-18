d5f885e1783f50e910b0ed5627cd0af693538a7a
Aaditya18 <61319634+Aaditya18@users.noreply.github.com>
2020-05-18 21:09:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var  database;
+var canvas,playerCount,form,player,game;
+var gameState=0
+
+function setup(){
+  database = firebase.database();
+ 
+ canvas= createCanvas(500,500);
+game=new Game()
+game.getState()
+game.start()
+}
+
+function draw(){
+  background("white");
+  
+}
+
